1730b120036285426e366b5bb81300cfa07315e7
Vanbasten01 <fouad.yasin01@gmail.com>
2023-11-05 23:37:34 +0100

    Filter places by Amenity

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,85 @@
+const $ = window.$;
+$(document).ready(function () {
+  $('.amenities h4').html('&nbsp;');
+  const amenityDict = {};
+  $('input[type=checkbox]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+    if ($(this).prop('checked')) {
+      amenityDict[amenityId] = amenityName;
+    } else {
+      delete amenityDict[amenityId];
+    }
+    /* if (amenityDict) {
+      console.log('full dict')
+    $('.amenities h4').text(Object.values(amenityDict).join(', '));
+    } else {
+      console.log('empty dict');
+      $('.amenities h4').html('&nbsp;');
+    }
+    */
+
+    if (Object.keys(amenityDict).length === 0) {
+      $('.amenities h4').html('&nbsp;');
+    } else {
+      $('.amenities h4').text(Object.values(amenityDict).join(', '));
+    }
+  });
+
+  function renderPlaces (data) {
+    const placesSection = $('section.places');
+    data.forEach(function (place) {
+      const article = $('<article>');
+      article.append('<div class="title_box">');
+      article.find('.title_box').append('<h2>' + place.name + '</h2>');
+      article.find('.title_box').append('<div class="price_by_night">$' + place.price_by_night + '</div>');
+      article.append('<div class="information">');
+      article.find('.information').append('<div class="max_guest">' + place.max_guest + ' Guest' + (place.max_guest != 1 ? 's' : '') + '</div>');
+      article.find('.information').append('<div class="number_rooms">' + place.number_rooms + ' Bedroom' + (place.number_rooms != 1 ? 's' : '') + '</div>');
+      article.find('.information').append('<div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + (place.number_bathrooms != 1 ? 's' : '') + '</div>');
+      article.append('<div class="user">');
+      article.append('<div class="description">' + place.description + '</div>');
+      placesSection.append(article);
+    });
+  }
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (data) {
+      renderPlaces(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(textStatus, errorThrown);
+    }
+  });
+  $('button').click(function () {
+    const idList = Object.keys(amenityDict);
+    const Dict = {};
+    Dict['amenities'] = idList;
+    console.log(idList.length);
+
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      contentType: 'application/json',
+      data: JSON.stringify(Dict),
+      success: function (data) {
+         $('section.places').empty();
+        renderPlaces(data);       
+        console.log(data);
+    }
+    });
+
+  });
+});
