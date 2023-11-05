6eceb2c2d809de7273ce818a0198dbc1d5460bce
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-11-05 23:13:46 +0100

    API status

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Flask Application """
+"""Flask Application"""
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -16,7 +16,7 @@ cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Close Storage """
+    """Close Storage"""
     storage.close()
 
 
